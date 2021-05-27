fccd2576fe9e97cf2409ceb3bb975265ed5f8aaf
wonho <wonho951@gmail.com>
2021-05-28 01:06:10 +0900

    2021/05/28 반복문 과제

---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,18 @@
+package com.javaex.practice;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int dan;
+		
+		for(i = 1; i <= 9; i++) {                  //세로 dan i
+			for(dan = 2; dan < 9; dan++) {                   //// 가로 i dan
+				System.out.print(dan + "*" + i + "=" + dan*i + "	");
+			}
+			System.out.println(dan + "*" + i + "=" + dan*i);
+		}
+	}
+
+}
