8c5bbbcacd3220a913d687315933d5294fc4ed7d
wonho <wonho951@gmail.com>
2021-05-29 00:02:08 +0900

    2021.05.29 반복문과제

--- src/com/javaex/practice/Ex18.java
+++ src/com/javaex/practice/Ex18.java
@@ -2,7 +2,7 @@ package com.javaex.practice;
 
 import java.util.Scanner;
 
-public class Ex17 {
+public class Ex18 {
 
 	public static void main(String[] args) {
 		
@@ -11,17 +11,26 @@ public class Ex17 {
 		
 		int num = sc.nextInt();
 		
-		
-		
 		for (int y = 1; y<=num; y++) {
 			
 			for (int x = y; x<=num; x++) {
 				System.out.print("*");
 			}
+			
 			System.out.println("");
 		}
-				
+		
+		for (int i = 1; i<num; i++) {
+			
+			for (int j = 0; j<=i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println("");
+		}	
+		
 		sc.close();
+		
 
 	}
 
---
+++ src/com/javaex/practice/Ex19.java
@@ -0,0 +1,49 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex19 {
+
+	public static void main(String[] args) {
+		
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = 0;		
+		int w = 0;
+		int save = 0;
+		
+		while (true) {
+			System.out.println("----------------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+			System.out.println("----------------------------------");
+			System.out.print("선택>");
+			int num = sc.nextInt();
+			
+			if(num==4) {
+				System.out.print("프로그램 종료");
+				break;
+			} else if (num==1) {
+				System.out.print("예금액>");
+				
+				save = sc.nextInt();
+				
+				a=a+save;
+			} else if (num == 2) {
+				System.out.print("출금액>");
+				w = sc.nextInt();
+				
+				a=save-w;
+			} else if ( num == 3) {
+				System.out.print("잔고액>" + a);
+			} else {
+				System.out.print("다시 입력해주세요.");
+			}
+			
+			System.out.println("");
+		}
+		
+		sc.close();
+	}
+
+}
